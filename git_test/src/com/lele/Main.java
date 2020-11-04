c37b80c4da112e6fecd06dab61725b926529878e
cjl <cjljava@163.com>
2020-11-04 23:33:14 +0800

    修改了main

--- git_test/src/com/lele/Main.java
+++ git_test/src/com/lele/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("1222222222222222");
+        System.out.println("rrrrrrrrrrrrrr");
     }
     public void show(){
 
